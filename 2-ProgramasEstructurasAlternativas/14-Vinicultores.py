f0f9413df7a9a35c6b8fa55b15cfce53e0c47371
alxcorp <alejandro@pfagot.com>
2022-10-23 12:45:01 +0200

    Update

--- 2-ProgramasEstructurasAlternativas/14-Vinicultores.py
+++ 2-ProgramasEstructurasAlternativas/14-Vinicultores.py
@@ -8,21 +8,20 @@ print("Este programa determina las ganancias del productor por la UVA")
 print("--------------------------------------------------------------")
 
 # Preguntamos las variables y comprobar entrada de datos
-grapeType = input("Ingrese el tipo de la uva(A o B): ").upper()
+grapeType = input("Ingrese el tipo de la uva (A o B): ").upper()
 
 
 if grapeType != "A" and grapeType != "B" or len(grapeType) != 1:
     print("Debes ingresar solamente A o B")
     quit()
 
-grapeSize = int(input("Ingrese el tamaño de la uva(1 o 2): "))
+grapeSize = int(input("Ingrese el tamaño de la uva (1 o 2): "))
 if grapeSize != 1 and grapeSize != 2:
     print("Debes ingresar solamente 1 o 2")
     quit()
 
-grapePrice = float(input("Ingrese del precio de la uva (Euros): "))
+grapePrice = float(input("Ingrese del precio de la uva (Euros): ")) * 100
 grapeAmount = float(input("Ingrese la cantidad de uva (Kg): "))
-
 
 if grapeType == "A":
     if grapeSize == 1:
@@ -33,7 +32,7 @@ else:
     if grapeSize == 1:
         grapePrice -= 30
     else:
-        grapePrice += 50
+        grapePrice -= 50
 
 
 total = grapeAmount * grapePrice
