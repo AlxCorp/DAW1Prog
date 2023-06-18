34c7b604dfef2630c02956dbf7483b841cd03c5c
AlxCorp <alejandro@pfagot.com>
2023-06-18 23:50:45 +0200

    Upload Files

--- 9-GUI/Calculadora/main.py
+++ 9-GUI/Calculadora/main.py
@@ -1,13 +1,102 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QWidget
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QGridLayout, QLineEdit, QPushButton, QVBoxLayout, )
+from functools import partial
 
-app = QApplication(sys.argv)
+ERROR_MSG = "ERROR"
+WINDOW_SIZE = 235
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40
 
-window = QWidget()
-window.setWindowTitle("Calculadora")
-window.setGeometry(100, 100, 280, 80)
-hello_message = QLabel("<h1>Hola Mundo!</h1>", parent=window)
-hello_message.move(60, 15)
 
-window.show()
-sys.exit(app.exec())
+class PyCalcWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("CalculadoraPython")
+        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.general_layout = QVBoxLayout()
+        central_widget = QWidget(self)
+        central_widget.setLayout(self.general_layout)
+        self.setCentralWidget(central_widget)
+        self._create_display()
+        self._create_buttons()
+
+    def _create_display(self):
+        self.display = QLineEdit()
+        self.display.setFixedSize(WINDOW_SIZE-15, DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.general_layout.addWidget(self.display)
+
+    def _create_buttons(self):
+        self.button_map = {}
+        buttons_layout = QGridLayout()
+        key_board = [
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+
+        for row, keys in enumerate(key_board):
+            for col, key in enumerate(keys):
+                self.button_map[key] = QPushButton(key)
+                self.button_map[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttons_layout.addWidget(self.button_map[key], row, col)
+
+        self.general_layout.addLayout(buttons_layout)
+
+    def set_display_text(self, text):
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def display_text(self):
+        return self.display.text()
+
+    def clear_display(self):
+        self.set_display_text("")
+
+
+def evaluate_expression(expression):
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+    return result
+
+
+class PyCalc:
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connect_signals_and_slots()
+
+    def _calculate_result(self):
+        result = self._evaluate(expression = self._view.display_text())
+        self._view.set_display_text(result)
+
+    def _build_expression(self, sub_expression):
+        if self._view.display_text() == ERROR_MSG:
+            self._view.clear_display()
+        expression = self._view.display_text() + sub_expression
+        self._view.set_display_text(expression)
+
+    def _connect_signals_and_slots(self):
+        for key_symbol, button in self._view.button_map.items():
+            if key_symbol not in {"=", "C"}:
+                button.clicked.connect(partial(self._build_expression, key_symbol))
+        self._view.button_map["="].clicked.connect(self._calculate_result)
+        self._view.display.returnPressed.connect(self._calculate_result)
+        self._view.button_map["C"].clicked.connect(self._view.clear_display)
+
+
+def main():
+    pycalc_app = QApplication([])
+    pycalc_window = PyCalcWindow()
+    pycalc_window.show()
+    PyCalc(model=evaluate_expression, view=pycalc_window)
+    sys.exit(pycalc_app.exec())
+
+
+if __name__ == "__main__":
+    main()
